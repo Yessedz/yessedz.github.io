10fe3415bfdb15c16ed61cdfbe26dd9ca2506606
yessedz <yessedzlz@email.com>
2022-09-27 18:08:22 -0500

    Helloworld.java

---
+++ parcial 1/tarea3/HelloWorld.java
@@ -0,0 +1,10 @@
+// Helloworld.java
+// Author:Yessenia Díaz 
+// Date: sep 26, 2022
+
+class HelloWorld{
+      public static void main(String[] args){
+           System.out.println("Hello, world!");
+  }
+}
+
