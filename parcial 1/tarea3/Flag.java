f08d1c195d53430ee228395495e2caa4b100ca64
yessedz <yessedzlz@email.com>
2022-10-04 18:33:25 -0500

    se agrego range.java

---
+++ parcial 1/tarea3/Flag.java
@@ -0,0 +1,15 @@
+/* 
+*Flag.java
+*Author: Yessedz
+*Date: Oct 4, 2022
+*Location: ixtac, ver. Mexico
+*/ 
+
+class Flag {
+  public static void main(String[] args) {
+
+    boolean flag = false;        
+    System.out.println("valor de flag:");      // define a boolean var
+    System.out.println(!!flag);      // prints true 
+    }
+}
