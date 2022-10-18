09cf9801ce0f0f29b54b17d2985976d2b7f4f869
yessedz <yessedzlz@gmail.com>
2022-10-18 17:48:35 -0500

    mis archivos

---
+++ parcial 1/tarea3/ForNumbers.java
@@ -0,0 +1,73 @@
+
+class ForNumbers {
+
+  public static void main(String[] args) {
+
+    
+   // Program to print numbers from 1 to 5
+   int n = 10;
+
+    // for loop
+
+    System.out.println("Tabla del 9");
+
+    for (int i = 9; i <= (n * 9); i=i+9) {
+
+      System.out.println(i);
+
+    }
+
+    System.out.println("Tabla del 9");
+
+    for (int i = 2; i <= n; i=i+2) {
+
+      System.out.println(i * 9);
+
+    }
+
+    System.out.println("pares menores a 100");
+  
+    int w=100;
+
+    for (int i= 2 ; i <=w; i=i+2 ) {
+        
+        System.out.println ("los pares son: "+i);
+    
+    }
+
+    int t= 50;
+
+    System.out.println("\n"+"Impares");
+
+    for (int y= 11 ; y<=t; y=y+2) {
+
+        System.out.println("los impares son: "+y);
+   
+    }
+    
+    
+    int j=0;
+
+    for (int g= 100 ; g  >=j; g=g-2 ) {
+        
+        System.out.println ("los pares son: "+g);
+    
+    }
+
+    System.out.println("\n"+"Impares");
+
+    int l= 11;
+
+    for (int y= 49; y>=l; y=y-2) {
+
+        System.out.println("los impares son: "+y);
+    }
+    
+
+
+
+
+
+  }
+
+}
