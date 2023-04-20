277e76a159f87be14aea0325e5ebf6cf3483260f
Yessedz <112595864+Yessedz@users.noreply.github.com>
2023-04-19 20:24:48 -0600

    Add files via upload

---
+++ Punto.java
@@ -0,0 +1,23 @@
+//package Ejercicios17Abril23;
+
+public class Punto{
+    private int X;
+    public void setX(int X){
+        this.X= X;
+    }
+    public int getX(){
+        return X;
+    }
+
+    private int Y;
+    public void setY(int Y){
+        this.Y= Y;
+    }
+    public int getY(){
+        return Y;
+    }
+    public String toString(){
+        return"(X:"+X+"|Y:"+Y+")";
+    }
+    
+}
